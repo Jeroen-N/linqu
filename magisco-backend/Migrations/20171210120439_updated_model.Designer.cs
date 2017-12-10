af4afe43a00a7d9f61b0384fbb349438053ee8ef
Jeroen <jeroennuyten@gmail.com>
2017-12-10 13:06:50 +0100

    Updated migrations.

--- magisco-backend/Migrations/20171210120439_updated_model.Designer.cs
+++ magisco-backend/Migrations/20171210120439_updated_model.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Magisco.Profile.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20171210120439_updated_model")]
+    partial class updated_model
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
